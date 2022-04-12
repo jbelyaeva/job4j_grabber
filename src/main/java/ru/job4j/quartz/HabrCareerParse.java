f0de206b856f7bbebd6fe905173b5c90a0274d72
julja83 <julja83@list.ru>
2022-04-12 13:24:31 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3

--- src/main/java/ru/job4j/quartz/HabrCareerParse.java
+++ src/main/java/ru/job4j/quartz/HabrCareerParse.java
@@ -12,20 +12,23 @@ public class HabrCareerParse {
 
   private static final String SOURCE_LINK = "https://career.habr.com";
 
-  private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+  private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
   public static void main(String[] args) throws IOException {
-    Connection connection = Jsoup.connect(PAGE_LINK);
-    Document document = connection.get();
-    Elements rows = document.select(".vacancy-card__inner");
-    rows.forEach(row -> {
-      Element dateElement = row.select(".vacancy-card__date").first();
-      String date = dateElement.child(0).attr("datetime");
-      Element titleElement = row.select(".vacancy-card__title").first();
-      Element linkElement = titleElement.child(0);
-      String vacancyName = titleElement.text();
-      String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-      System.out.printf("%s %s %s%n", vacancyName, link, date);
-    });
+
+    for (int countPage = 1; countPage < 6; countPage++) {
+      Connection connection = Jsoup.connect(PAGE_LINK + countPage);
+      Document document = connection.get();
+      Elements rows = document.select(".vacancy-card__inner");
+      rows.forEach(row -> {
+        Element dateElement = row.select(".vacancy-card__date").first();
+        String date = dateElement.child(0).attr("datetime");
+        Element titleElement = row.select(".vacancy-card__title").first();
+        Element linkElement = titleElement.child(0);
+        String vacancyName = titleElement.text();
+        String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+        System.out.printf("%s %s %s%n", vacancyName, link, date);
+      });
+    }
   }
 }
