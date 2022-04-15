4fee2920a4bc29b2294d3201738390eea5c14fb3
julja83 <julja83@list.ru>
2022-04-15 06:40:25 +0300

    2.2. Модель данных - Post

---
+++ src/main/java/ru/job4j/quartz/Post.java
@@ -0,0 +1,47 @@
+package ru.job4j.quartz;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Post {
+
+  private int id;
+  private String title;
+  private String link;
+  private String description;
+  private LocalDateTime created ;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Post)) {
+      return false;
+    }
+    Post post = (Post) o;
+    return id == post.id && Objects.equals(title, post.title) && Objects.equals(
+        link, post.link) && Objects.equals(description, post.description);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (title != null ? title.hashCode() : 0);
+    result = 31 * result + (link != null ? link.hashCode() : 0);
+    result = 31 * result + (description != null ? description.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", Post.class.getSimpleName() + "[", "]")
+        .add("id=" + id)
+        .add("title='" + title + "'")
+        .add("link='" + link + "'")
+        .add("description='" + description + "'")
+        .add("created=" + created)
+        .toString();
+  }
+}
